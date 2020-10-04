0dec88e992743acc8bb35a6a749a2917d7ff9ea1
Bohdan Romanchenko <rov4ak3@gmail.com>
2020-10-04 19:05:01 +0300

    Completed task

---
+++ Home_Work/Lab_Work_2.1/ILibraryUser.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab_Work_2._1
+{
+    interface ILibraryUser
+    {
+        public void AddBook(string name);
+        public void RemoveBook(string name);
+        public string BookInfo(int i);
+        public int BooksCount();
+    }
+}
---
+++ Home_Work/Lab_Work_2.1/LibraryUser.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab_Work_2._1
+{
+    class LibraryUser : ILibraryUser
+    {
+        public readonly string FirstName;
+        public readonly string LastName;
+        public readonly int Id;
+        public string Phone { get; set; }
+        public readonly int BookLimit;
+        private List<string> bookList { get; set; } = new List<string>();
+
+        public string this[int i] => bookList[i];
+
+        public LibraryUser()
+        {
+            FirstName = "Vasya";
+            LastName = "Pupkin";
+            Phone = "+380968364924";
+            Id = 1;
+        }
+
+        public LibraryUser(string FirstName, string LastName, string Phone, int Id)
+        {
+            this.FirstName = FirstName;
+            this.LastName = LastName;
+            this.Phone = Phone;
+            this.Id = Id;
+        }
+
+        public void AddBook(string nameBook)
+        {
+            bookList.Add(nameBook);
+        }
+        public void RemoveBook(string nameBook)
+        {
+            bookList.Remove(nameBook);
+        }
+        public string BookInfo(int i)
+        {
+           return bookList[i];
+        }
+        public int BooksCount()
+        {
+            return bookList.Count;
+        }
+    }
+}
